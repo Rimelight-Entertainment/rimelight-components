ef3a71eb78ac9721f6cecc40bb285934d0902eec
github <88442616+idantitydotme@users.noreply.github.com>
2025-10-23 05:19:33 -0300

    Updates callout info size

--- playground/app/components/Fix.vue
+++ playground/app/components/Fix.vue
@@ -11,7 +11,7 @@
   <!-- prettier-ignore -->
   <div class="w-full w-fit sm:w-fit md:w-fit lg:w-fit xl:w-fit w-auto w-1 w-2 w-3 w-4 w-6 w-8 w-10 w-12 w-16 w-20 w-24 w-32 w-36 xl:w-36 w-40 w-48 xl:w-48 w-56 w-64 w-96 w-128 w-256 lg:w-1 lg:w-2 lg:w-3 lg:w-4 lg:w-6 lg:w-8 lg:w-10 lg:w-12 lg:w-16 lg:w-20 lg:w-24 lg:w-32 lg:w-40 lg:w-48 lg:w-56 lg:w-64 lg:w-96 lg:w-128 lg:w-256"/>
   <!-- prettier-ignore -->
-  <div class="size-full size-fit size-auto size-1 size-2 size-3 size-4 size-6 size-8 size-10 size-12 size-16 size-20 size-24 size-32 size-40 size-48 size-56 size-64 size-96 size-128 size-256 lg:size-1 lg:size-2 lg:size-3 lg:size-4 lg:size-6 lg:size-8 lg:size-10 lg:size-12 lg:size-16 lg:size-20 lg:size-24 lg:size-32 lg:size-40 lg:size-48 lg:size-56 lg:size-64 lg:size-96 lg:size-128 lg:size-256"/>
+  <div class="size-full size-fit size-auto size-1 size-2 size-3 size-4 size-4.5 size-6 size-8 size-10 size-12 size-16 size-20 size-24 size-32 size-40 size-48 size-56 size-64 size-96 size-128 size-256 lg:size-1 lg:size-2 lg:size-3 lg:size-4 lg:size-6 lg:size-8 lg:size-10 lg:size-12 lg:size-16 lg:size-20 lg:size-24 lg:size-32 lg:size-40 lg:size-48 lg:size-56 lg:size-64 lg:size-96 lg:size-128 lg:size-256"/>
   <!-- prettier-ignore -->
   <div class="p-1 p-2 p-3 p-4 p-6 p-8 p-10 p-12 p-16 p-20 p-24 p-32 p-40 p-48 p-56 p-64 p-96 p-128 p-256 lg:p-1 lg:p-2 lg:p-3 lg:p-4 lg:p-6 lg:p-8 lg:p-10 lg:p-12 lg:p-16 lg:p-20 lg:p-24 lg:p-32 lg:p-40 lg:p-48 lg:p-56 lg:p-64 lg:p-96 lg:p-128 lg:p-256"/>
   <!-- prettier-ignore -->
