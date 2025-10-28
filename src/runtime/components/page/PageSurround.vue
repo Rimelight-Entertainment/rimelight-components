06d6657fded2d92af5c40be8a3e484ad6d915f09
github <88442616+idantitydotme@users.noreply.github.com>
2025-10-28 02:24:42 -0300

    Adds page surround component

---
+++ src/runtime/components/page/PageSurround.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const {
+  pageType,
+  previousTitle,
+  previousDescription,
+  previousTo,
+  nextTitle,
+  nextDescription,
+  nextTo
+} = defineProps<{
+  pageType: string
+  previousTitle?: string
+  previousDescription?: string
+  previousTo?: string
+  nextTitle?: string
+  nextDescription?: string
+  nextTo?: string
+}>()
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
+    <ULink v-if="previousTitle" :to="previousTo">
+      <UCard
+        variant="outline"
+        class="hover:bg-elevated/50 focus-visible:outline-primary group block bg-transparent"
+      >
+        <div class="gap-md flex flex-col">
+          <div class="gap-xs flex flex-col">
+            <UButton
+              variant="outline"
+              icon="lucide:arrow-left"
+              class="group-hover text-primary group-hover:text-highlighted w-fit rounded-full"
+            />
+            <span class="text-muted">
+              {{ $t("navigation_previous") }}
+              {{ $t(pageType) }}
+            </span>
+          </div>
+          <div class="gap-xs flex flex-col">
+            <p class="text-primary group-hover:text-highlighted">
+              {{ previousTitle }}
+            </p>
+            <p class="text-toned">
+              {{ previousDescription }}
+            </p>
+          </div>
+        </div>
+      </UCard>
+    </ULink>
+    <ULink v-if="nextTitle" :to="nextTo">
+      <UCard
+        variant="outline"
+        class="hover:bg-elevated/50 focus-visible:outline-primary group block bg-transparent"
+      >
+        <div class="gap-md flex flex-col items-end">
+          <div class="gap-xs flex flex-col items-end">
+            <UButton
+              variant="outline"
+              icon="lucide:arrow-right"
+              class="text-primary group-hover:text-highlighted w-fit rounded-full"
+            />
+            <span class="text-muted">
+              {{ $t("navigation_next") }}
+              {{ $t(pageType) }}</span
+            >
+          </div>
+          <div class="gap-xs flex flex-col items-end">
+            <p class="text-primary group-hover:text-highlighted">
+              {{ nextTitle }}
+            </p>
+            <p class="text-toned">
+              {{ nextDescription }}
+            </p>
+          </div>
+        </div>
+      </UCard>
+    </ULink>
+  </div>
+</template>
