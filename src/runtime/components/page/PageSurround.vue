9900e566d51cc72fc580d1a8d6916d15775299c2
github <88442616+idantitydotme@users.noreply.github.com>
2025-10-29 11:17:08 -0300

    Fixes surround spacing with empty cards

--- src/runtime/components/page/PageSurround.vue
+++ src/runtime/components/page/PageSurround.vue
@@ -20,61 +20,65 @@ const {
 
 <template>
   <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
-    <ULink v-if="previousTitle" :to="previousTo" class="h-full">
-      <UCard
-        variant="outline"
-        class="hover:bg-elevated/50 focus-visible:outline-primary group block h-full bg-transparent"
-      >
-        <div class="gap-md flex flex-col">
-          <div class="gap-xs flex flex-col">
-            <UButton
-              variant="outline"
-              icon="lucide:arrow-left"
-              class="group-hover text-primary group-hover:text-highlighted w-fit rounded-full"
-            />
-            <span class="text-muted">
-              {{ $t("navigation_previous") }}
-              {{ $t(pageType) }}
-            </span>
+    <div>
+      <ULink v-if="previousTitle" :to="previousTo" class="h-full">
+        <UCard
+          variant="outline"
+          class="hover:bg-elevated/50 focus-visible:outline-primary group block h-full bg-transparent"
+        >
+          <div class="gap-md flex flex-col">
+            <div class="gap-xs flex flex-col">
+              <UButton
+                variant="outline"
+                icon="lucide:arrow-left"
+                class="group-hover text-primary group-hover:text-highlighted w-fit rounded-full"
+              />
+              <span class="text-muted">
+                {{ $t("navigation_previous") }}
+                {{ $t(pageType) }}
+              </span>
+            </div>
+            <div class="gap-xs flex flex-col">
+              <p class="text-primary group-hover:text-highlighted">
+                {{ previousTitle }}
+              </p>
+              <p class="text-toned">
+                {{ previousDescription }}
+              </p>
+            </div>
           </div>
-          <div class="gap-xs flex flex-col">
-            <p class="text-primary group-hover:text-highlighted">
-              {{ previousTitle }}
-            </p>
-            <p class="text-toned">
-              {{ previousDescription }}
-            </p>
+        </UCard>
+      </ULink>
+    </div>
+    <div>
+      <ULink v-if="nextTitle" :to="nextTo" class="h-full">
+        <UCard
+          variant="outline"
+          class="hover:bg-elevated/50 focus-visible:outline-primary group block h-full bg-transparent"
+        >
+          <div class="gap-md flex flex-col items-end">
+            <div class="gap-xs flex flex-col items-end">
+              <UButton
+                variant="outline"
+                icon="lucide:arrow-right"
+                class="text-primary group-hover:text-highlighted w-fit rounded-full"
+              />
+              <span class="text-muted">
+                {{ $t("navigation_next") }}
+                {{ $t(pageType) }}</span
+              >
+            </div>
+            <div class="gap-xs flex flex-col items-end">
+              <p class="text-primary group-hover:text-highlighted text-right">
+                {{ nextTitle }}
+              </p>
+              <p class="text-toned text-right">
+                {{ nextDescription }}
+              </p>
+            </div>
           </div>
-        </div>
-      </UCard>
-    </ULink>
-    <ULink v-if="nextTitle" :to="nextTo" class="h-full">
-      <UCard
-        variant="outline"
-        class="hover:bg-elevated/50 focus-visible:outline-primary group block h-full bg-transparent"
-      >
-        <div class="gap-md flex flex-col items-end">
-          <div class="gap-xs flex flex-col items-end">
-            <UButton
-              variant="outline"
-              icon="lucide:arrow-right"
-              class="text-primary group-hover:text-highlighted w-fit rounded-full"
-            />
-            <span class="text-muted">
-              {{ $t("navigation_next") }}
-              {{ $t(pageType) }}</span
-            >
-          </div>
-          <div class="gap-xs flex flex-col items-end">
-            <p class="text-primary group-hover:text-highlighted text-right">
-              {{ nextTitle }}
-            </p>
-            <p class="text-toned text-right">
-              {{ nextDescription }}
-            </p>
-          </div>
-        </div>
-      </UCard>
-    </ULink>
+        </UCard>
+      </ULink>
+    </div>
   </div>
 </template>
