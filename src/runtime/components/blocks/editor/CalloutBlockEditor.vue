edb08ca99ccf5807ea40d819ef64bc278d9b5c0e
github <88442616+idantitydotme@users.noreply.github.com>
2025-12-01 17:26:54 -0300

    Adds debug component

---
+++ src/runtime/components/blocks/editor/CalloutBlockEditor.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { CalloutBlockProps } from "~~/src/runtime/types/blocks"
+
+const { variant, children, to, target } = defineProps<CalloutBlockProps>()
+</script>
+
+<template>
+  <div class="p-8 bg-error-500">
+  <RCCallout :variant="variant" :to="to" :target="target">
+    <RCBlockRenderer :blocks="children" />
+  </RCCallout>
+  </div>
+</template>
