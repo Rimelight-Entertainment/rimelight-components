820051ccfb1fe8e0df6e304c620bd0a0bda032b7
github <88442616+idantitydotme@users.noreply.github.com>
2025-12-04 03:08:44 -0300

    Updates packages

--- src/runtime/utils/index.ts
+++ src/runtime/utils/index.ts
@@ -1,12 +1,3 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export type ObjectValues<T> = T[keyof T]
-
 /**
  * Converts a string into a URL-friendly slug.
  * - Converts to lowercase.
