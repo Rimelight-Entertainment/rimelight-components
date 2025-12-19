160a6e6c0360f4691e5712ee732fd241e398291a
github <88442616+idantitydotme@users.noreply.github.com>
2025-12-19 17:57:48 -0300

    updates package exports

--- build.config.ts
+++ build.config.ts
@@ -3,18 +3,12 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
     entries: [
         './src/module',
-        { input: './src/runtime/composables/index', outDir: './dist/composables', name: 'index' },
-        { input: './src/runtime/utils/index', outDir: './dist/utils', name: 'index' },
-        { input: './src/runtime/locale/index', outDir: './dist/locale', name: 'index' },
-    ],
-    declaration: true,
-    externals: [
-        'nuxt',
-        '@nuxt/schema',
-        '@nuxt/kit',
-        '@nuxt/ui',
-        'vue',
-        'defu',
-        'tailwind-variants'
+        {
+            builder: 'mkdist',
+            input: './src/runtime',
+            outDir: './dist/runtime',
+            format: 'esm',
+            declaration: true
+        }
     ]
 })
