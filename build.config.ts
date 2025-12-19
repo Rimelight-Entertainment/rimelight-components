8fab598b49a7da2cb58ebfae0a693cfdd4f0e4e9
github <88442616+idantitydotme@users.noreply.github.com>
2025-12-19 18:04:22 -0300

    updates package exports

--- build.config.ts
+++ build.config.ts
@@ -1,14 +1,9 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-    entries: [
-        './src/module',
-        {
-            builder: 'mkdist',
-            input: './src/runtime',
-            outDir: './dist/runtime',
-            format: 'esm',
-            declaration: true
+    hooks: {
+        'mkdist:entry:options'(ctx, entry, options) {
+            options.addRelativeDeclarationExtensions = false
         }
-    ]
+    },
 })
