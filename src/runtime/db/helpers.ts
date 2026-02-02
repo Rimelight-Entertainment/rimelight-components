269d81e8098d2be25696ed9b043bed70fd6270ab
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-02-02 12:29:00 -0300

    renames db timestamp helpers

--- src/runtime/db/helpers.ts
+++ src/runtime/db/helpers.ts
@@ -4,9 +4,9 @@ import { sql } from "drizzle-orm"
 export const id = uuid("id").default(sql`uuidv7()`).notNull()
 
 export const timestamps = {
-  updated_at: timestamp("updated_at", {
+  updatedAt: timestamp("updated_at", {
     withTimezone: true
   }).$onUpdate(() => new Date()),
-  created_at: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
-  deleted_at: timestamp("deleted_at", { withTimezone: true })
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+  deletedAt: timestamp("deleted_at", { withTimezone: true })
 }
