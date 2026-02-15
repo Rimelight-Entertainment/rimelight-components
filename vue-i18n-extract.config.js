edca54216d2d75ea38c4949a7c849f17e07bce55
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-02-14 23:49:47 -0300

    chore(i18n): update vue-i18n-extract configuration and scripts

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "vueFiles": "./{auth,components,composables,db,internal,playground,server,utils}/**/*.?(js|vue|ts)",
+  "languageFiles": "./locales/*.?(json|yaml|yml|js)",
+  "exclude": [],
+  "output": false,
+  "add": false,
+  "remove": false,
+  "ci": false,
+  "separator": ".",
+  "noEmptyTranslation": ""
+}
