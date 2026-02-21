5a9baebf67ffe215a03c0ee58f7fb636d5d72f78
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-02-21 15:11:28 -0300

    feat: updates article views

---
+++ app/components/article/views/index.ts
@@ -0,0 +1,3 @@
+export { default as PageEditView } from "rimelight-components/app/components/article/views/PageEditView.vue";
+export { default as PageReviewView } from "rimelight-components/app/components/article/views/PageReviewView.vue";
+export { default as PageLiveView } from "rimelight-components/app/components/article/views/PageLiveView.vue";
